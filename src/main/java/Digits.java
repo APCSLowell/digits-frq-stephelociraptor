f289c2f84e721796c9166a17123838cd367a6f71
stephelociraptor <139821295+stephelociraptor@users.noreply.github.com>
2024-04-15 10:08:41 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,7 +7,7 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    digitList = new ArrayList <Integer>();
+		digitList = new ArrayList<Integer>();
 		if (num == 0){
 			digitList.add(0);
 		}
@@ -21,7 +21,7 @@ public class Digits
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		for (int i = 0; i < digitList.size() -1; i++){
+		for (int i = 0; i < digitList.size()-1; i++){
 			if (digitList.get(i) > digitList.get(i+1){
 				return false; 
 			}
